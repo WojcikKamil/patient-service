524ee71b9f23cc22ca31c8e24973280e4f94a387
Maria Urban <51886150+Ethalya@users.noreply.github.com>
2021-05-25 10:30:47 +0200

    env

---
+++ src/main/java/pl/wsiz/iid3/healthcenter/HealthCenterApplication.java
@@ -0,0 +1,13 @@
+package pl.wsiz.iid3.healthcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wsiz/iid3/healthcenter/HealthCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wsiz.iid3.healthcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
